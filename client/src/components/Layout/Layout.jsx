ad2594edb1cde820f6677780a22a7f48b7787a03
12Ahn22 <12ayo22@gmail.com>
2023-02-18 11:24:08 +0900

    Feat: 기본 레이아웃 UI완성

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from 'react-router';
+
+import Headers from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
+
+import styles from './Layout.module.css';
+
+function Layout() {
+  return (
+    <>
+      <Headers />
+      <div className={styles.container}>
+        <Sidebar />
+        <main className={styles.main}>
+          <h2>메인메인메인메인메인</h2>
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
