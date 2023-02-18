41ac79da0a698e9167a53648e652b1339f7bed70
12Ahn22 <12ayo22@gmail.com>
2023-02-18 09:17:12 +0900

    Feat: Header UI 작업 완성

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,33 +1,38 @@
 import { BiSearch } from 'react-icons/bi';
+
 import Input from '../../UI/Input/Input';
-import Button from '../../UI/Button/Button';
+import SubNavList from './SubNav/SubNavList';
+import MainNav from './MainNav/MainNav';
+
 import styles from './Header.module.css';
 
 function Header() {
+  const subName = [
+    { url: '/', text: 'About' },
+    { url: '/', text: 'Products' },
+    { url: '/', text: 'For Teams' },
+  ];
+
   return (
-    <header>
-      {/* LOGO */}
-      <a href="/">StackOverflow</a>
-      {/* LIST */}
-      <ul>
-        <li>
-          <a href="/">About</a>
-        </li>
-        <li>
-          <a href="/">Products</a>
-        </li>
-      </ul>
-      {/* SEARCH */}
-      <Input placeholder={'Search...'} isValid leftIcon={<BiSearch />} />
-      {/* NAV */}
-      <ul>
-        <li>
-          <Button text="Log In" size="small" color="border" />
-        </li>
-        <li>
-          <Button text="Sign up" size="small" />
-        </li>
-      </ul>
+    <header className={styles.container}>
+      <div className={styles.header}>
+        {/* LOGO */}
+        <a className={styles.logo_container} href="/">
+          <span className={styles.logo}>StackOverflow</span>
+        </a>
+        {/* LIST */}
+        <ul className={`${styles.nav} ${styles['sub_nav']}`}>
+          {subName.map((sub) => (
+            <SubNavList key={sub.text} {...sub} />
+          ))}
+        </ul>
+        {/* SEARCH */}
+        <Input placeholder={'Search...'} isValid leftIcon={<BiSearch />} />
+        {/* NAV */}
+        <ul className={`${styles.nav} ${styles['main_nav']}`}>
+          <MainNav isLogin />
+        </ul>
+      </div>
     </header>
   );
 }
