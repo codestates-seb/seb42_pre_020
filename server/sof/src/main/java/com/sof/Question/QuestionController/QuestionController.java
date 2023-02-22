237bb300508d98a5df2c171f342365230bdb34aa
BO0461 <bo0461@naver.com>
2023-02-22 09:10:55 +0900

    Feat:질문 수정 삭제기능 추가

--- server/sof/src/main/java/com/sof/Question/QuestionController/QuestionController.java
+++ server/sof/src/main/java/com/sof/Question/QuestionController/QuestionController.java
@@ -1,10 +1,12 @@
 package com.sof.Question.QuestionController;
 
+import com.nimbusds.oauth2.sdk.token.AccessToken;
 import com.sof.Exception.UnauthorizedException;
 import com.sof.Question.QuestionDto.QuestionDto;
 import com.sof.Question.QuestionEntity.QuestionEntity;
 import com.sof.Question.QuestionMapper.QuestionMapper;
 import com.sof.Question.QuestionService.QuestionService;
+import com.sof.Security.Dto.TokenDto;
 import org.aspectj.weaver.patterns.TypePatternQuestions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +28,7 @@ public class QuestionController {
         this.questionService = questionService;
         this.questionMapper = questionMapper;
     }
+
     //글작성
     @PostMapping("/questions")
     public ResponseEntity AddQuestions(@Validated @RequestBody QuestionDto.Post addQ) {
@@ -43,20 +46,47 @@ public class QuestionController {
 
 
     }
+
     @GetMapping("/questions")
-    public  ResponseEntity allQuestion(){
+    public ResponseEntity allQuestion() {
         List<QuestionEntity> questions = this.questionService.getQuestions();
 
         return new ResponseEntity<>
-                (new ResponseDto(questionMapper.questionsToQuestionResponse(questions)),HttpStatus.OK);
+                (new ResponseDto(questionMapper.questionsToQuestionResponse(questions)), HttpStatus.OK);
 
-        //수정기능 컨트롤러(update)
-        //@patchMapping("/question")
 
-        //삭제기능 컨트롤러(delete)
-        //@DeleteMapping
+    }
 
-        //
+    @PatchMapping("/questions/{questionId}")
+    public ResponseEntity updateQuestion(@PathVariable("questionId") Long Id,
+                                         @RequestBody QuestionDto.Patch patchDto) {
+        QuestionEntity question = questionService.update(Id, patchDto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+        //메퍼를써서 dto를 Entity로 바꾸고
+        //엔티티를 서비스로 보낸다.
+        //서비스에서 돌아온 엔티티를 다시 메퍼로 리스폰스 dto로 바꾸고
+        //리스폰스 엔티티에 넣어서 보내준다.
+
+        //질문 상세페이지 질문 id get하기
+
+    }
+    @DeleteMapping("/delete/{questionId}")
+    public ResponseEntity deleteQuestion(@PathVariable("questionId") Long Id,
+                                         @RequestBody TokenDto requestBody) {
+
+        //if문에는 본인이 아닌 작성한 질문이 아닐경우 처리 할 예외처리
+        if(!questionService.){
+        throw new UnauthorizedException("작성자가 아닙니다.");
+        }
+            //이 질문을 id기준으로 찾아야 함
+        QuestionEntity question = this.questionService.find(Id);
+        this.questionService.delete(Id);
+
+        return  new ResponseEntity(HttpStatus.OK);
+
+
 
     }
 }
